2ac09cea15937b3a507f6046f32b3efb56d56fb7
Komal Batra <KomalBatra@gmail.com>
2018-10-14 23:37:08 -0400

    significant updates

---
+++ assets/javascript/mainjs.js
@@ -0,0 +1,16 @@
+
+
+/*----------------------------------------------------*/
+	/*	Modal Popup
+	------------------------------------------------------*/
+    $(document).ready(function(){
+        function modalOne () {
+            $('#modal-01').modal();
+        };
+   
+    $(document).on('click', '#first-one', modalOne);
+ 
+   });
+
+
+   
